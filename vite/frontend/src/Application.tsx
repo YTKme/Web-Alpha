774ebde64373ab30d74b6e54a3c219ac9a7dc33b
Yan Kuang <YTKme@Outlook.com>
2023-12-08 15:26:47 -0800

    Add ModeToggle for Bootstrap

--- vite/frontend/src/Application.tsx
+++ vite/frontend/src/Application.tsx
@@ -4,11 +4,13 @@
  */
 
 import './Application.css'
+import { ModeToggleBootstrap } from './component/ModeToggle/ModeToggleBootstrap'
 
 function Application() {
   return (
     <>
       <h1>Hello Vite Alpha!</h1>
+      <ModeToggleBootstrap />
     </>
   )
 }
