7ac6ebe7f30af1a51c6b40a8c9b2739ee986cd9c
Yan Kuang <YTKme@Outlook.com>
2023-12-08 00:59:12 -0800

    Add ThemeToggle component for testing

--- vite/frontend/src/component/Navigation.tsx
+++ vite/frontend/src/component/Navigation.tsx
@@ -8,17 +8,15 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 import viteLogo from '/res/img/vite.svg'
-import { ThemeToggle } from './ThemeToggle'
 
 
 export default function Navigation() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="tw-bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <ThemeToggle />
+          <div className="tw-mx-auto tw-max-w-7xl tw-px-2 tw-sm:px-6 tw-lg:px-8">
+            <div className="tw-relative tw-flex tw-h-16 tw-items-center tw-justify-between">
             </div>
           </div>
         </>
