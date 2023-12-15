7b910b50cf1d49055b5311a2a1ebe04981f19f4b
Yan Kuang <YTKme@Outlook.com>
2023-12-14 21:06:35 -0800

    Update ModeToggleBootstrap component for vite frontend

--- vite/frontend/src/component/ModeToggle/ModeToggleBootstrap.tsx
+++ vite/frontend/src/component/ModeToggle/ModeToggleBootstrap.tsx
@@ -15,50 +15,50 @@ export default function ModeToggleBootstrap() {
       <div className='dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle'>
         {/* Toggle */}
         <button
-          id='toggle'
+          id='bd-theme'
           className='btn dropdown-toggle py-2 d-flex align-items-center'
           type='button'
           data-bs-toggle='dropdown'
           aria-expanded='false'
           aria-label='Toggle Mode (Auto)'
         >
-          <svg className='bi' width='1em' height='1em' fill='currentColor'>
+          <svg className='bi theme-icon-active my-1' width='1em' height='1em' fill='currentColor'>
             <use href={`${BootstrapIcon}#circle-half`} />
           </svg>
-          <span className='visually-hidden'>Toggle Theme</span>
+          <span id='bd-theme-text' className='visually-hidden'>Toggle Theme</span>
         </button>
         {/* Mode */}
-        <ul id='mode' className='dropdown-menu shadow'>
+        <ul className='dropdown-menu dropdown-menu-end shadow' aria-labelledby='bd-theme-text'>
           <li>
-            <button className='dropdown-item d-flex align-items-center' data-bs-theme-value='light' type='button'>
-              <svg className='bi me-2' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#sun-fill`} />
+            <button className='dropdown-item d-flex align-items-center' type='button' data-bs-theme-value='light' aria-pressed='false'>
+              <svg className='bi theme-icon me-2' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#sun-fill`}></use>
               </svg>
               Light
-              <svg className='bi ms-auto d-none' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#check2`} />
+              <svg className='bi ms-auto d-none' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#check2`}></use>
               </svg>
             </button>
           </li>
           <li>
-            <button className='dropdown-item d-flex align-items-center' data-bs-theme-value='dark' type='button'>
-              <svg className='bi me-2' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#moon-fill`} />
+            <button className='dropdown-item d-flex align-items-center' type='button' data-bs-theme-value='dark' aria-pressed='false'>
+              <svg className='bi theme-icon me-2' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#moon-fill`}></use>
               </svg>
               Dark
-              <svg className='bi ms-auto d-none' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#check2`} />
+              <svg className='bi ms-auto d-none' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#check2`}></use>
               </svg>
             </button>
           </li>
           <li>
-            <button className='dropdown-item d-flex align-items-center' data-bs-theme-value='auto' type='button'>
-              <svg className='bi me-2' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#circle-half`} />
+            <button className='dropdown-item d-flex align-items-center active' type='button' data-bs-theme-value='auto' aria-pressed='true'>
+              <svg className='bi theme-icon me-2' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#circle-half`}></use>
               </svg>
               Auto
-              <svg className='bi ms-auto d-none' width='1em' height='1em' fill='currentColor'>
-                <use href={`${BootstrapIcon}#check2`} />
+              <svg className='bi ms-auto d-none' width='1em' height='1em'>
+                <use href={`${BootstrapIcon}#check2`}></use>
               </svg>
             </button>
           </li>
