9eac528177cccbac6f7213bca260604da043babc
Yan Kuang <YTKme@Outlook.com>
2023-12-19 01:30:05 -0800

    Add StyleX project

---
+++ stylex/frontend/tailwind.config.ts
@@ -0,0 +1,21 @@
+/**
+ * tailwindcss Configuration
+ */
+
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  prefix: 'tw-',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
+export default config
