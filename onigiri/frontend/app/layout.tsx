0848dcd6b22c944dfd0b8313012eaf7431aff4c7
Yan Kuang <YTKme@Outlook.com>
2024-06-17 06:55:15 -0700

    Update onigiri frontend layout and style

--- onigiri/frontend/app/layout.tsx
+++ onigiri/frontend/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode,
 }) {
   return (
-    <html lang='en' data-bs-theme='light'>
+    <html lang='en'>
     <body>
       {children}
     </body>
