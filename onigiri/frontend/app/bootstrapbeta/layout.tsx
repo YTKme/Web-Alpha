835c01f742b924847ddf57edc10e79272669bf71
Yan Kuang <YTKme@Outlook.com>
2024-07-30 12:00:26 -0700

    Add onigiri frontend Bootstrap Beta component and context

---
+++ onigiri/frontend/app/bootstrapbeta/layout.tsx
@@ -0,0 +1,35 @@
+/**
+ * Bootstrap Alpha Layout
+ */
+
+import { ReactNode } from 'react';
+
+import { Viewport } from 'next';
+
+// Context
+import BootstrapBetaProvider from '@/context/BootstrapBetaContext';
+
+// CSS
+import './bootstrapbeta.scss';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
+export default function BootstrapLayout({
+  children,
+}: {
+  children: ReactNode,
+}) {
+  return (
+    <html lang='en'>
+      <body>
+        <BootstrapBetaProvider>
+          {children}
+        </BootstrapBetaProvider>
+      </body>
+    </html>
+  )
+}
