7ede4382ae5b527e8908505cef8aac779ddc870f
Yan Kuang <YTKme@Outlook.com>
2024-08-15 18:10:09 -0700

    Update onigiri frontend Bootstrap Alpha app, component, and context

--- onigiri/frontend/component/BootstrapAlphaColorMode.tsx
+++ onigiri/frontend/component/BootstrapAlphaColorMode.tsx
@@ -4,61 +4,73 @@
 
 'use client';
 
-import CircleHalf from 'bootstrap-icons/icons/circle-half.svg';
+import { useBootstrapAlphaColorMode } from '@/context/BootstrapAlphaColorModeContext';
 
-import { useBootstrapAlphaColorMode } from "@/context/BootstrapAlphaColorModeContext";
+import Check2 from 'bootstrap-icons/icons/check2.svg';
+import CircleHalf from 'bootstrap-icons/icons/circle-half.svg';
+import MoonFill from 'bootstrap-icons/icons/moon-fill.svg';
+import SunFill from 'bootstrap-icons/icons/sun-fill.svg';
 
 export default function BootstrapAlphaColorMode() {
   // Use Context
   const { colorModeAlpha, setColorModeAlpha } = useBootstrapAlphaColorMode();
 
   return (
-    <div>
-      <p>Current Theme:</p>
+    <div className='dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle'>
+      {/* Toggle */}
       <button
-        type="button"
-        className="btn btn-light m-1"
-        onClick={() => setColorModeAlpha('light')}
-        disabled={colorModeAlpha === 'light'}
+        id='bd-theme'
+        className='btn dropdown-toggle py-2 d-flex align-items-center'
+        type='button'
+        data-bs-toggle='dropdown'
+        aria-expanded='false'
+        aria-label='Toggle Mode (System)'
       >
-        Light
+        <CircleHalf className='bi my-1 theme-icon-active' width='1em' height='1em' />
+        <span id='bd-theme-text' className='visually-hidden'>Toggle Mode</span>
       </button>
-      <button
-        type="button"
-        className="btn btn-dark m-1"
-        onClick={() => setColorModeAlpha('dark')}
-        disabled={colorModeAlpha === 'dark'}
-      >
-        Dark
-      </button>
-      <button
-        type="button"
-        className="btn btn-secondary m-1"
-        onClick={() => setColorModeAlpha('auto')}
-        disabled={colorModeAlpha === 'auto'}
-      >
-        System
-      </button>
-
-      <div>
-        <CircleHalf width='2em' />
-        <CircleHalf width={16} height={16} />
-        <CircleHalf width={32} height={32} />
-      </div>
-
-      <div className="dropdown">
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="true">
-          Default dropdown
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
-          <li><a className="dropdown-item active" href="#">Action</a></li>
-          <li><a className="dropdown-item" href="#">Action</a></li>
-          <li><a className="dropdown-item" href="#">Another action</a></li>
-          <li><a className="dropdown-item" href="#">Something else here</a></li>
-          <li><hr className="dropdown-divider" /></li>
-          <li><a className="dropdown-item" href="#">Separated link</a></li>
-        </ul>
-      </div>
+      {/* Mode */}
+      <ul className='dropdown-menu dropdown-menu-end shadow' aria-labelledby='bd-theme-text'>
+        <li>
+          <button
+            className='dropdown-item d-flex align-items-center'
+            type='button'
+            data-bs-theme-value='light'
+            aria-pressed='false'
+            onClick={() => setColorModeAlpha('light')}
+          >
+            <SunFill className='bi me-2' width='1em' height='1em' />
+            Light
+            <Check2 className='bi ms-auto d-none' width='1em' height='1em' />
+          </button>
+        </li>
+        <li>
+          <button
+            className='dropdown-item d-flex align-items-center'
+            type='button'
+            data-bs-theme-value='dark'
+            aria-pressed='false'
+            onClick={() => setColorModeAlpha('dark')}
+          >
+            <MoonFill className='bi me-2' width='1em' height='1em' />
+            Dark
+            <Check2 className='bi ms-auto d-none' width='1em' height='1em' />
+          </button>
+        </li>
+        <li>
+          <button
+            className='dropdown-item d-flex align-items-center active'
+            type='button'
+            data-bs-theme-value='system'
+            aria-pressed='true'
+            onClick={() => setColorModeAlpha('system')}
+          >
+            <CircleHalf className='bi me-2' width='1em' height='1em' />
+            System
+            <Check2 className='bi ms-auto d-none' width='1em' height='1em' />
+          </button>
+        </li>
+      </ul>
     </div>
   );
-}
+};
