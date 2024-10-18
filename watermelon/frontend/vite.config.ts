3302d8c93538e076d506e311face22f536d9e467
Yan Kuang <YTKme@Outlook.com>
2024-10-18 11:26:30 -0700

    Restructure watermelon project

--- watermelon/frontend/vite.config.ts
+++ watermelon/frontend/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
+
+  // Mode
+  console.debug('Mode:', mode)
 
   return {
     // Environment Variable
