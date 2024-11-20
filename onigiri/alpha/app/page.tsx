a1d97dd92c503dc698dbfb0e777a73128d888b88
Yan Kuang <YTKme@Outlook.com>
2024-11-19 23:20:54 -0800

    Update onigiri alpha layout and page

--- onigiri/alpha/app/page.tsx
+++ onigiri/alpha/app/page.tsx
@@ -2,14 +2,7 @@
  * Root Page
  */
 
-import type { Metadata } from 'next'
-
 import Login from "@/component/Login/Login";
-
-export const metadata: Metadata = {
-  title: '🍙 Onigiri 🍙',
-  description: '🍙 Onigiri 🍙',
-}
 
 export default function Page() {
   return (
